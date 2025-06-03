85e15924f8a0aa1542778b4dd8565fe4b67f1484
Ariel <jaroviskyariel@gmail.com>
2025-06-03 14:13:54 -0300

    nuevo cambios

--- mail/main.js
+++ mail/main.js
@@ -6,7 +6,7 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     const servicio = document.getElementById("servicio").value;
     const mensaje = document.getElementById("mensaje").value;
 
-    const numero = "5491154616161"; // <-- cambiá por tu número con código de país, sin + ni 0
+    const numero = "5491136179702"; // <-- cambiá por tu número con código de país, sin + ni 0
     const texto = `Nombre y apellido: ${nombre}%0AEmail: ${email}%0AServicio: ${servicio}%0AMensaje: ${mensaje}`;
 
     const url = `https://wa.me/${numero}?text=${texto}`;
